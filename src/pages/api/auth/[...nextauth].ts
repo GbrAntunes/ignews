a15692d100a4a92494a45c789b340c17e45c5c1e
Gabriel Antunes <gbr.antunes@gmail.com>
2021-04-06 10:05:33 -0400

    feat: create user on first login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
+import { signIn } from 'next-auth/client'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,6 +14,41 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  // jwt: {
+  //   signingKey: process.env.SIGNING_KEY
+  // },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
 
-  database: process.env.DATABASE_URL,
+      try {
+        await fauna.query(
+          q.If( // If
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ), // true
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ), // false
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  },
 })
