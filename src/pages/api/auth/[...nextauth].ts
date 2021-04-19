33fdce0689a0b2e16cdf4046567f03ac9f7b98f6
Gabriel Antunes <gbr.antunes@gmail.com>
2021-04-19 12:29:19 -0400

    feat: Post page done

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import { query as q } from 'faunadb'
 
 import NextAuth from 'next-auth'
-import { signIn } from 'next-auth/client'
+import { session, signIn } from 'next-auth/client'
 import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna'
@@ -14,10 +14,45 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
-  // jwt: {
-  //   signingKey: process.env.SIGNING_KEY
-  // },
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                'active'
+              )
+            ])
+          )
+        )
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      } 
+
+      
+    },
     async signIn(user, account, profile) {
       const { email } = user
 
